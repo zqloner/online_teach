0f63497908a40d21b5bd00385d6c546e3c5ae920
zqloner <zqloner@163.com>
2021-05-28 15:19:18 +0800

    ...完成图表展示的前端部分

---
+++ guli_parent/canal_client/src/main/java/com/atguigu/canal/CanalApplication.java
@@ -0,0 +1,30 @@
+package com.atguigu.canal;
+
+import com.atguigu.canal.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/5/2814:17
+ * @Company: MGL
+ */
+@SpringBootApplication
+public class CanalApplication implements CommandLineRunner {
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        //项目启动，执行canal客户端监听
+        canalClient.run();
+    }
+}
