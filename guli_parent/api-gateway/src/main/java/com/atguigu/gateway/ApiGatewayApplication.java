2f48393dc312589ec6e3d5dfa28d4348887ce0d6
zqloner <zqloner@163.com>
2021-05-28 16:18:59 +0800

    ...网关模块构建

---
+++ guli_parent/api-gateway/src/main/java/com/atguigu/gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/5/2815:49
+ * @Company: MGL
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
